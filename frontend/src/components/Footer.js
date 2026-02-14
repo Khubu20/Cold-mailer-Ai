c9649a8f90636bbb30c8f2deb333477c399c8a9b
Khushboo Gupta <khubu.gupta20@gmail.com>
2026-02-15 01:31:52 +0530

    updated cold mailer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -5,8 +5,8 @@ const Footer = () => {
   return (
     <footer className="footer">
       <p>ColdMailer © 2025</p>
-      {/* <p>akshatfarkya07@gmail.com</p> */}
-      <a href='mailto:akshatfarkya07@gmail.com' className='footerMail'>akshatfarkya07@gmail.com</a>
+      {/* <p>khubu.gupta20@gmail.com</p> */}
+      <a href='mailto:khubu.gupta20@gmail.com' className='footerMail'>khubu.gupta20@gmail.com</a>
     </footer>
   );
 };
